0ba06c97df9d7b2c97a09108935d6ab2a225653d
pablogalvil <pablogalvil53@gmail.com>
2023-10-17 13:10:40 +0200

    cambios2

--- Programación2324/src/unidad2/Ejemplo7Loteria.java
+++ Programación2324/src/unidad2/Ejemplo7Loteria.java
@@ -1,5 +1,7 @@
 package unidad2;
 
+import java.util.Scanner;
+
 public class Ejemplo7Loteria {
 
 	/**
@@ -16,8 +18,82 @@ public class Ejemplo7Loteria {
 	 * @param args
 	 */
 
+	public static final int NUMEROS_COINCIDEN = 0;
+	public static final int NUMEROS_NO_COINCIDEN = 1;
+	public static final int NUMEROS_COINCIDEN2 = 2;
+	public static final int NUMEROS_NO_COINCIDEN2 = 3;
+
+	public static final int RESULTADO_GANADOR = 0;
+	public static final int RESULTADO_NO_GANADOR = 1;
+	public static final int RESULTADO_GANADOR2 = 2;
+	public static final int RESULTADO_NO_GANADOR2 = 3;
+
 	public static void main(String[] args) {
 
-	}
+		int num1 = 0;
+		int num2 = 0;
+		int loteria = 0;
+		int resultado = RESULTADO_NO_GANADOR;
+		String nom1 = "";
+		String nom2 = "";
+		Scanner teclado = new Scanner(System.in);
 
+		System.out.print("Bienvenido usuario 1, por favor introduzca su nombre : ");
+		nom1 = teclado.next();
+		System.out.print(nom1 + " por favor introduzca su número : ");
+		num1 = teclado.nextInt();
+
+		System.out.print("Bienvenido usuario 2, por favor introduzca su nombre : ");
+		nom2 = teclado.next();
+		System.out.print(nom2 + " por favor introduzca su número : ");
+		num2 = teclado.nextInt();
+
+		loteria = (int) (Math.random() * 2);
+
+		switch (num1) {
+		case NUMEROS_NO_COINCIDEN:
+			if (num1 != loteria) {
+				resultado = RESULTADO_NO_GANADOR;
+			}
+			break;
+		case NUMEROS_COINCIDEN:
+			if (num1 == loteria) {
+				resultado = RESULTADO_GANADOR;
+			}
+			break;
+		}
+		switch (num2) {
+		case NUMEROS_NO_COINCIDEN2:
+			if (num2 != loteria) {
+				resultado = RESULTADO_NO_GANADOR2;
+			}
+			break;
+		case NUMEROS_COINCIDEN2:
+			if (num2 == loteria) {
+				resultado = RESULTADO_GANADOR2;
+			}
+			break;
+		}
+
+		if (resultado == RESULTADO_NO_GANADOR && resultado == RESULTADO_NO_GANADOR2) {
+			System.out.println("Lo sentimos, " + nom1 + " y " + nom2 + " ninguno de ustedes ha consiguido el premio.");
+		} else {
+			if (resultado == RESULTADO_GANADOR && resultado == RESULTADO_NO_GANADOR2) {
+				System.out.println("¡Felicidades, " + nom1 + " usted ha ganado el premio!");
+				System.out.println("Lo sentimos, " + nom2 + " usted no ha tenido la misma suerte :(");
+			} else {
+				if (resultado == RESULTADO_GANADOR2 && resultado == RESULTADO_NO_GANADOR) {
+					System.out.println("¡Felicidades, " + nom2 + " usted ha ganado el premio!");
+					System.out.println("Lo sentimos, " + nom1 + " usted no ha tenido la misma suerte :(");
+				} else {
+					if (resultado == RESULTADO_GANADOR && resultado == RESULTADO_GANADOR2) {
+						System.out.println("¡Felicidades, " + nom1 + " y " + nom2 + " ambos han ganado el premio!");
+					}
+				}
+			}
+
+		}
+
+		teclado.close();
+	}
 }
---
+++ Programación2324/src/unidad2/Ejemplo8AciertaNumero.java
@@ -0,0 +1,19 @@
+package unidad2;
+
+public class Ejemplo8AciertaNumero {
+
+	/**
+	 * El programa genera un número aleatorio entre 1 y 19 y el usuario lo acertara
+	 * en 3 intentos o explotara una bomba enganchada a su cuerpo. El programa
+	 * diabolico en cada fallo le dirá si el número a acertar es mayor o menor y le
+	 * metera presión. En el primer fallo hay un 30% de posibilidades de que la
+	 * maquina le mienta.
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+	}
+
+}

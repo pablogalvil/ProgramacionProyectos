d9b2455ff8f09402268a8e5704d5f95605cf285b
pablogalvil <pablogalvil53@gmail.com>
2023-10-21 10:14:58 +0200

    cambios2.4

--- PracticasProgramación1/src/unidad2/ejercicio7.java
+++ PracticasProgramación1/src/unidad2/ejercicio7.java
@@ -6,24 +6,19 @@ public class ejercicio7 {
 
 	public static void main(String[] args) {
 		int diasMes = 0;
-		boolean esBisiesto = false;
 
 		Scanner teclado = new Scanner(System.in);
 		System.out.print("Introduce el número de días del mes : ");
 		diasMes = teclado.nextInt();
-		System.out.print("¿El año es bisiesto? ");
-		esBisiesto = teclado.nextBoolean();
+		
+		diasMes=diasMes%2;
 
 		switch (diasMes) {
-		case 4:
-			if (esBisiesto == true)
-				System.out.println("Estas en febrero");
-			break;
 		case 0:
-			System.out.println("Estas en Febrero, Abril, Junio, Septiembre o Noviembre");
+			System.out.println("Los pares son Febrero(si no es bisiesto), Abril, Junio, Septiembre y Noviembre");
 			break;
 		case 1:
-			System.out.println("Estas en Enero, Marzo, Mayo, Julio, Agosto, Octubre o Diciembre");
+			System.out.println("Los impares son Enero, Febrero(si es bisiesto), Marzo, Mayo, Julio, Agosto, Octubre y Diciembre");
 			break;
 		}
 
--- PracticasProgramación1/src/unidad2/ejercicio7b.java
+++ PracticasProgramación1/src/unidad2/ejercicio7b.java
@@ -6,13 +6,18 @@ public class ejercicio7b {
 
 	public static void main(String[] args) {
 		int diasMes = 0;
-		boolean esBisiesto = false;
 
 		Scanner teclado = new Scanner(System.in);
 		System.out.print("Introduce el número de días del mes : ");
 		diasMes = teclado.nextInt();
-		System.out.print("¿El año es bisiesto? ");
-		esBisiesto = teclado.nextBoolean();
+		
+		diasMes=diasMes%2;
+		
+		if (diasMes==0) {
+			System.out.println("Los pares son Febrero(si no es bisiesto), Abril, Junio, Septiembre y Noviembre");
+		}else {
+			System.out.println("Los impares son Enero, Febrero(si es bisiesto), Marzo, Mayo, Julio, Agosto, Octubre y Diciembre");
+		}
 
 		teclado.close();
 

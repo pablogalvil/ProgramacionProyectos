cf2774f3757995474059296e0ffcf55e03189831
pablogalvil <pablogalvil53@gmail.com>
2023-10-24 12:58:40 +0200

    cambios 2.6

---
+++ PracticasProgramación1/src/unidad2/ejercicio10.java
@@ -0,0 +1,27 @@
+package unidad2;
+
+public class ejercicio10 {
+	/**
+	 * 
+	 * Realizar un programa que lea la edad, si está casado (boolean), el sexo
+	 * (char), comunidad autonoma, numero de hijos y el sueldo anual, y muestre las
+	 * ayudas anuales al alquiler calculandolas segun las siguientes condiciones
+	 * 
+	 * Si pertenecen a la comunidad autónoma de Pais Vasco o Valencia o tienen un
+	 * sueldo superior a 60000 y más de 2 hijos, su ayuda será un 12% de su sueldo
+	 * anual + pluses.
+	 * 
+	 * Si pertenece a cantabria, andalucia o Madrid, es mujer y sueldo entre 55000 y
+	 * 45000, su ayuda sera un 13% del sueldo + pluses.
+	 * 
+	 * plus1: Si cumplen que estan casados con más de 3 hijos, cobran un fijo de
+	 * 100€ mes. plus2: Cualquiera que tenga mas de 5 hijos cobrará 2000€ anuales.
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- PracticasProgramación1/src/unidad2/ejercicio9.java
+++ PracticasProgramación1/src/unidad2/ejercicio9.java
@@ -1,9 +1,42 @@
 package unidad2;
 
+import java.util.Scanner;
+
 public class ejercicio9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String distancia = "";
+		char unidad = 'A';
+		int rango = 0;
+		double numDist = 0;
+		double resultado = 0;
+
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.print("Introduce la distancia en metros (solo la primera letra de la unidad siendo m, h o k) : ");
+		distancia = teclado.next().toLowerCase();
+
+		// Creo el rango para poder separar el número de la letra.
+		rango = (distancia.length() - 1);
+		unidad = distancia.charAt(rango);
+		distancia = distancia.substring(0, rango);
+		// Paso el número de String a double
+		numDist = Double.parseDouble(distancia);
+
+		if (unidad == 'k') {
+			resultado = numDist * 0.621371;
+			System.out.println("Tu número es " + resultado + " millas.");
+		} else if (unidad == 'h') {
+			resultado = numDist * 0.0621371;
+			System.out.println("Tu número es " + resultado + " millas.");
+		} else if (unidad == 'm') {
+			resultado = numDist * 0.000621371;
+			System.out.println("Tu número es " + resultado + " millas.");
+		} else {
+			System.out.println("La unidad introducida no es válida.");
+		}
+
+		teclado.close();
 
 	}
 

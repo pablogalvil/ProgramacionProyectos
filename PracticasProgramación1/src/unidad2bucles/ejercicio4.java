b815654cfdf8e0db84fc303dbdefa35152016501
pablogalvil <pablogalvil53@gmail.com>
2023-11-17 13:03:24 +0100

    cambios4.1

--- PracticasProgramación1/src/unidad2bucles/ejercicio4.java
+++ PracticasProgramación1/src/unidad2bucles/ejercicio4.java
@@ -8,34 +8,63 @@ public class ejercicio4 {
 		// Variables
 		String oracion = "";
 		Scanner teclado = new Scanner(System.in);
+		// Para que no se resetee el número de letras en el bucle que guarda la cadena
+		int numLetra = 0;
 
 		// Pedimos la oración
 		System.out.print("Introduce tu oración : ");
 		oracion = teclado.nextLine();
 
 		// Separamos la string para convertirla en array
-		String[] cadena = new String[100];
+		char[] array = {};
 
 		// Variable para el total de palabras con más de 5 letras.
+		int numPalabras = 0;
 
 		// Bucle para la longitud de las palabras.
 		for (int i = 0; i < oracion.length(); i++) {
-			int j = 0;
-			while (j < oracion.length()) {
+			System.out.println(i);
+			for (int j = 0; j < oracion.length(); j++) {
+				j += numLetra;
 				if (oracion.charAt(j) != ' ') {
-					cadena[i] += oracion.charAt(j);
+					array[i] += oracion.charAt(j);
 				} else {
+					numLetra++;
 					break;
 				}
 			}
 		}
+		String[] cadena = asda;
+
+		for (int z = 0; z < cadena.length; z++) {
+			// Segundo bucle para invertir las palabras
+			if (cadena[z].length() > 5) {
+				numPalabras++;
+			} else {
+				continue;
+			}
+		}
+
 		// Mostramos cuantas palabras con más de 5 letras hay en la oración.
-		System.out.println(cadena[2]);
+		System.out.println("Hay " + numPalabras + " palabras con más de 5 letras");
 
 		// Segunda parte, mostrar las palabras invertidas
 
 		// Bucle para pasar por cada palabra, igual que el anterior
 		// Usare x e y para las variables
+		for (int x = 0; x < cadena.length; x++) {
+			// Segundo bucle para invertir las palabras
+			if (cadena[x].length() > 5) {
+				for (int y = cadena[x].length() - 1; y >= 0; y--) {
+					// Variable para cambiar las letras
+					char invertirLetra = cadena[x].charAt(y);
+					System.out.print(invertirLetra);
+				}
+			} else {
+				continue;
+			}
+			System.out.print(" ");
+		}
 
 		teclado.close();
 	}
--- PracticasProgramación1/src/unidad2bucles/ejercicio5.java
+++ PracticasProgramación1/src/unidad2bucles/ejercicio5.java
@@ -4,18 +4,9 @@ public class ejercicio5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int x = 0; x < cadena.length; x++) {
-			// Segundo bucle para invertir las palabras
-			if (cadena[x].length() > 5) {
-				for (int y = cadena[x].length() - 1; y >= 0; y--) {
-					// Variable para cambiar las letras
-					char invertirLetra = cadena[x].charAt(y);
-					System.out.print(invertirLetra);
-				}
-			} else {
-				continue;
-			}
-			System.out.print(" ");
+
+		for (int i = 0; i < 10; i++) {
+
 		}
 
 	}

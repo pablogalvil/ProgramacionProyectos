92fed130ec7127988f0d16e39d83365eadf0b483
pablogalvil <pablogalvil53@gmail.com>
2023-11-24 12:12:28 +0100

    asd

---
+++ PracticasProgramación1/src/unidad2bucles/ejercicio9.java
@@ -0,0 +1,16 @@
+package unidad2bucles;
+
+public class ejercicio9 {
+
+	public static final int SIZE = 8;
+	public static final char TOWN = 'T';
+	public static final char ROAD = 'R';
+	public static final char CURSED = 'D';
+	public static final char MONSTER = 'M';
+
+	public static void main(String[] args) {
+		char[][] mapaReino = new char[SIZE][SIZE];
+
+	}
+
+}
